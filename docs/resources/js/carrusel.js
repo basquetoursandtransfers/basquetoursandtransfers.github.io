26844c1e28ae85321f929d90adad87a6bd9bd1a3
Leticia <leticiasilveirmendoza@gmail.com>
2025-06-16 13:28:38 +0200

    Modificanco carrusel solo en docs

--- docs/resources/js/carrusel.js
+++ docs/resources/js/carrusel.js
@@ -56,9 +56,9 @@ const swiper = new Swiper(".mySwiper", {
         prevEl: ".swiper-button-prev"
     },
     breakpoints: {
-        0: {slidesPerView: 1},
-        992: {slidesPerView: 3},
-        768: {slidesPerView: 2},
-        576: {slidesPerView: 1}
+        0: {slidesPerView: 1, spaceBetween: 10},
+        576: {slidesPerView: 1, spaceBetween: 10},
+        768: {slidesPerView: 2, spaceBetween: 20},
+        992: {slidesPerView: 3, spaceBetween: 30}
     }
 });
